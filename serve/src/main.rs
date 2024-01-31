33dcd7eef679b2b870e36b42f3ecddd193e4c73e
Zach Krzyzanowski <zkrzyz@gmail.com>
2024-01-30 22:52:57 -0500

    more minimal repro

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -1,16 +1,20 @@
-use axum::{error_handling::HandleErrorLayer, http::StatusCode, routing::post, BoxError, Router};
+use axum::{
+    debug_handler, error_handling::HandleErrorLayer, http::StatusCode, response::IntoResponse,
+    response::Response, routing::post, BoxError, Router,
+};
+use std::fmt;
 use std::time::Duration;
 use tower::{timeout::TimeoutLayer, ServiceBuilder};
 use tower_http::catch_panic::CatchPanicLayer;
 
-mod routes;
-use routes::route;
+// comment this out after uncommenting CustomError code below
+use error::error::CustomError;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .layer(CatchPanicLayer::new())
-        .route("/test", post(route::run))
+        .route("/test", post(run))
         .layer(
             ServiceBuilder::new()
                 // this middleware goes above `TimeoutLayer` because it will receive
@@ -25,3 +29,29 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+#[debug_handler]
+pub async fn run(_body: String) -> Result<String, CustomError> {
+    Ok("".to_string())
+}
+
+// #[derive(Debug)]
+// pub struct CustomError(anyhow::Error);
+
+// impl fmt::Display for CustomError {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "")
+//     }
+// }
+
+// impl std::error::Error for CustomError {}
+
+// impl IntoResponse for CustomError {
+//     fn into_response(self) -> Response {
+//         (
+//             StatusCode::INTERNAL_SERVER_ERROR,
+//             format!("Something went wrong: {}", self.0),
+//         )
+//             .into_response()
+//     }
+// }
